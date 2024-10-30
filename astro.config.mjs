e9a37b4cbc51c6d5a84a532ea75c7e09d12e7d89
Vinícius Reis <viniciusreis3105@gmail.com>
2024-10-30 16:32:55 -0300

    feat: add auth with expo and configure midleware for protect pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
+import node from "@astrojs/node";
+import tailwind from "@astrojs/tailwind";
+import clerk from "@clerk/astro";
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	integrations: [tailwind(), clerk()],
+	adapter: node({ mode: "standalone" }),
+	output: "server",
+});
